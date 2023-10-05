ba1bf0bd69edc1ab9796fe61b82ad24c6662423c
PavloDymohlo <pavelanatolevic777@gmail.com>
2023-10-05 20:15:48 +0300

    add code in Hw2Task5

--- src/Hw2Task5.java
+++ src/Hw2Task5.java
@@ -1,13 +1,20 @@
 import java.util.Scanner;
 
 public class Hw2Task5 {
-    public static void main(String[] args) {
+
+    public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter your number");
         String number = scan.nextLine();
 
-        for(int i = number.length()-1; i >= 0; i-- ){
+        String change = "";
+        for (int i = number.length(); i-- > 0;) {
+            change += number.charAt(i);
             System.out.print(number.charAt(i));
         }
+        System.out.println();
+        System.out.println("we can use this number "+change);
+        // нашел более надежные и интересные методы что-бы перевернуть число и потом использовать его,
+        //но решил остановится на том, что мы уже знаем)
     }
 }
