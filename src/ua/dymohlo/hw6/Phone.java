5593594c5ebda956db2ef60754daa8be8e03145f
PavloDymohlo <pavelanatolevic777@gmail.com>
2023-10-18 21:40:51 +0300

    add Phone, Task 2 in hw6

---
+++ src/ua/dymohlo/hw6/Phone.java
@@ -0,0 +1,77 @@
+package ua.dymohlo.hw6;
+
+import java.util.Arrays;
+
+/*
+Класс Phone.
+а) Создайте класс Phone, который содержит переменные(поля) number,
+model и weight.
+б) Добавить конструктор в класс Phone, который принимает на вход
+три параметра для инициализации переменных класса - number, model
+и weight.
+в) Добавить конструктор, который принимает на вход два параметра
+для инициализации переменных класса - number, model.
+г) Добавить конструктор без параметров.
+д) Создайте три экземпляра этого класса.
+ж) Присвоить значения полям класса.
+з) Выведите на консоль значения их полей.
+и) Добавить в класс Phone методы: receiveCall, имеет один параметр
+– имя звонящего. Выводит на консоль сообщение “Звонит {name}”.
+getNumber – возвращает номер телефона. Вызвать эти методы для
+каждого из объектов.
+*/
+public class Phone {
+    int number;
+    String model;
+    double weight;
+
+    Phone() {
+    }
+
+    Phone(int number, String model) {
+        this.number = number;
+        this.model = model;
+    }
+
+    Phone(int number, String model, double weight) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+
+    public static void main(String[] args) {
+        Phone firstPhone = new Phone(101, "Nokia", 192.5);
+        System.out.println(firstPhone);
+        firstPhone.receiveCall("Nokia");
+        firstPhone.getNumber(101);
+
+        Phone secondPhone = new Phone(102, "Samsung", 197.3);
+        System.out.println(secondPhone);
+        secondPhone.receiveCall("Samsung");
+        secondPhone.getNumber(102);
+
+        Phone thirdPhone = new Phone(103, "Lenovo", 175.3);
+        System.out.println(thirdPhone);
+        thirdPhone.receiveCall("Lenovo");
+        thirdPhone.getNumber(103);
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "number=" + number +
+                ", model='" + model + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+
+    public void receiveCall(String call) {
+        this.model = call;
+        System.out.println("Calling " + call);
+    }
+
+    public void getNumber(int numberPhone) {
+        this.number = numberPhone;
+        System.out.println("The number is calling you: " + numberPhone);
+    }
+}
---
+++ src/ua/dymohlo/hw6/Task2.java
@@ -0,0 +1,30 @@
+package ua.dymohlo.hw6;
+
+/*
+Найти и исправить ошибку в коде с помощью дебаггера
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        int[][] triangle = new int[5][];
+        triangle[0] = new int[1];
+        triangle[1] = new int[2];
+        triangle[2] = new int[3];
+        triangle[3] = new int[4];
+        triangle[4] = new int[5];
+        for (int i = 0; i < triangle.length; i++) {
+            System.out.println(triangle.length + "!");
+            System.out.println("i = " + i);
+            for (int j = 0; j < triangle[i].length; j++) {
+                triangle[i][j] = 0;
+            }
+            System.out.println(triangle[i].length);
+        }
+    }
+}
+/*
+Смотря на вывод в консоль предположил, что ошибка состоит в том, что при каждой итерации( кроме первой) выводяться
+лишние элементы. Проблему в коде определил следующим образом: с помощью Line breakpoint пробежался по коду начиная
+с первого цикла и обнаружил что ошибка находится во втором цикле. println выводит результат после каждой итерации
+цикла, в следствии чего на консоль выводятся лишние элементы в количестве зависящем от количества итераций внутри
+второго цикла. Проблему решил выносом строки "System.out.println(triangle[i].length);" за пределы цикла.
+*/
