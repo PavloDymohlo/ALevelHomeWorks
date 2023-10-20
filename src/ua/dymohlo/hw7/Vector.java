6a60753b42e0fc6e4a7f83be5f61113cd1183936
PavloDymohlo <pavelanatolevic777@gmail.com>
2023-10-20 18:49:44 +0300

    correct Vector in hw7

--- src/ua/dymohlo/hw7/Vector.java
+++ src/ua/dymohlo/hw7/Vector.java
@@ -20,11 +20,11 @@ import java.util.concurrent.ThreadLocalRandom;
 */
 public class Vector {
 
-    int x;
-    int y;
-    int z;
+    double x;
+    double y;
+    double z;
 
-    public Vector(int x, int y, int z) {
+    public Vector(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
@@ -32,48 +32,57 @@ public class Vector {
 
     public static void main(String[] args) {
         Vector vectorA = new Vector(5, 6, 7);
-        int[] vectors = new int[5];
-        System.out.println(vectorA.vectorLength());
-        System.out.println(vectorA.vectorMultiplication());
-        System.out.println(vectorA.cosineBetweenVectors());
-        System.out.println(vectorA.sumVector());
-        System.out.println(vectorA.difVector());
-        System.out.println(Arrays.toString(vectors(5)));
+        Vector vectorB = new Vector(1, 3, 4);
+        Vector[] vectors = new Vector[3];
+        System.out.println("Vector's length is: " + vectorA.vectorLength());
+        System.out.println("Vector product of two vectors is: " + vectorA.vectorMultiplication(vectorB));
+        System.out.println("cosine of the angle is; " + vectorA.cosineBetweenVectors(vectorB));
+        System.out.println("The sum of vectors is: " + vectorA.sumVector(vectorB));
+        System.out.println("The different of vectors is: " + vectorA.difVector(vectorB));
+        System.out.println("New vectors are: " + Arrays.toString(vectors(3)));
+    }
+
+    @Override
+    public String toString() {
+        return "Vector{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
     }
 
     public double vectorLength() {
         return Math.sqrt((x * x) + (y * y) + (z * z));
     }
 
-    public double vectorMultiplication() {
-        Vector vectorB = new Vector(2, 1, 4);
-        return Math.sqrt((x * x) + (y * y) + (z * z)) * Math.sqrt((vectorB.x * vectorB.x) + (vectorB.y * vectorB.y)
-                + (vectorB.z * vectorB.z));
+    public Vector vectorMultiplication(Vector vectorB) {
+        double pointX = y * vectorB.z - z * vectorB.y;
+        double pointY = z * vectorB.x - x * vectorB.z;
+        double pointZ = x * vectorB.y - y * vectorB.x;
+        return new Vector(pointX, pointY, pointZ);
     }
 
-    public double cosineBetweenVectors() {
-        Vector vectorB = new Vector(2, 1, 4);
+    public double cosineBetweenVectors(Vector vectorB) {
         return (x * vectorB.x + y * vectorB.y + z * vectorB.z) / (Math.sqrt((x * x) + (y * y) + (z * z)) *
                 Math.sqrt((vectorB.x * vectorB.x) + (vectorB.y * vectorB.y) + (vectorB.z * vectorB.z)));
     }
 
-    public double sumVector() {
-        Vector vectorB = new Vector(2, 1, 4);
-        return (Math.sqrt((x * x) + (y * y) + (z * z))) + (Math.sqrt((vectorB.x * vectorB.x) +
-                (vectorB.y * vectorB.y) + (vectorB.z * vectorB.z)));
+    public Vector sumVector(Vector vectorB) {
+        return new Vector(x + vectorB.x, y + vectorB.y, z + vectorB.z);
     }
 
-    public double difVector() {
-        Vector vectorB = new Vector(2, 1, 4);
-        return (Math.sqrt((x * x) + (y * y) + (z * z))) - (Math.sqrt((vectorB.x * vectorB.x) +
-                (vectorB.y * vectorB.y) + (vectorB.z * vectorB.z)));
+    public Vector difVector(Vector vectorB) {
+        return new Vector(x - vectorB.x, y - vectorB.y, z - vectorB.z);
     }
 
-    public static int[] vectors(int n) {
-        int[] randVectors = new int[n];
-        for (int i = 0; i < randVectors.length; i++) {
-            randVectors[i] = ThreadLocalRandom.current().nextInt(1, 100);
+    public static Vector[] vectors(int n) {
+        Vector[] vectorC = new Vector[n];
+        for (int i = 0; i < vectorC.length; i++) {
+            double pointA = ThreadLocalRandom.current().nextDouble(1, 10);
+            double pointB = ThreadLocalRandom.current().nextDouble(1, 10);
+            double pointC = ThreadLocalRandom.current().nextDouble(1, 10);
+            vectorC[i] = new Vector(pointA, pointB, pointC);
         }
-        return randVectors;
+        return vectorC;
     }
 }
