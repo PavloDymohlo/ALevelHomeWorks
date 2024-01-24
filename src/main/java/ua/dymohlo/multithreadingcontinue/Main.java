7115ec41ab43c810897aaa196e47ddd0578987e6
PavloDymohlo <pavelanatolevic777@gmail.com>
2024-01-24 23:09:29 +0200

    correct Main

--- src/main/java/ua/dymohlo/multithreadingcontinue/Main.java
+++ src/main/java/ua/dymohlo/multithreadingcontinue/Main.java
@@ -2,6 +2,9 @@ package ua.dymohlo.multithreadingcontinue;
 
 public class Main {
     public static void main(String[] args) {
-        
+        AtomicSynchronize.IncrementAtomicInteger();
+        IncrementWithoutSynchronize.incrementWithoutSynchronize();
+        IncrementWithSynchronize.incrementWithSynchronize();
+        ProducerConsumerProblemWIthQueue.problemWIthQueue();
     }
 }
